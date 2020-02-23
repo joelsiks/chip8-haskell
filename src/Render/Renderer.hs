1486c0b67e4d67ee26be502006829e749011ceff
JoXW100 <joxw100@gmail.com>
2020-02-23 17:20:37 +0100

    Added some code documentation and removed "for" as it was not used.

--- src/Render/Renderer.hs
+++ src/Render/Renderer.hs
@@ -7,11 +7,28 @@ import Graphics.Gloss.Interface.IO.Game
 import Data.Word
 import Data.ByteString (ByteString, pack) -- Same as a bitmap
 
+{-  Represents the type of a function that handles user inputs for a game.
+    When a game is started it requires some function that handles inputs, that function has this type
+    INVARIANT: used for functions asociated with startRenderer
+-}
 type HandleKeyFunc = Char -> CPU -> CPU
-type UpdateFunc = Float -> CPU -> CPU
-type RendererFunc = CPU -> [[Bool]]
 
+{-  Represents the type of function that handles update calls from a game
+    When a game is started it requires some function that handles what to do every frame, that function has this type
+    INVARIANT: used for functions asociated with startRenderer
+-}
+type UpdateFunc    = Float -> CPU -> CPU
 
+{-  Represents the type of function that handles conversion from a CPU state to an image
+    When a game is started it requires some function that handles how to convert a CPU state into an image, that function has this type
+    INVARIANT: used for functions asociated with startRenderer
+-}
+type RendererFunc  = CPU -> [[Bool]]
+
+{-  Represents a holder for all display variables
+    Stores variables used for the creaton of a display
+    INVARIANT: used for functions asociated with startRenderer
+-}
 data DisplaySettings = Settings
     {
         name       :: String,
@@ -19,8 +36,12 @@ data DisplaySettings = Settings
         background :: Color,
         fps        :: Int
     } deriving (Show)
-st = Settings "Game" (64,32) blue 60
 
+{-  createFrame settings pixels
+    converts the given pixels into a renderer readable picture
+    PRE: The number of pixels is equal to the number of pixels required for the given screen size from settings
+    RETURNS: A renderer readable picture created from the given pixels
+-}
 createFrame :: DisplaySettings -> [[Bool]] -> Picture
 createFrame s pixels = bitmapOfByteString sizeX sizeY (BitmapFormat (TopToBottom) (PxRGBA)) bitmapData False
     where
@@ -37,26 +58,36 @@ createFrame s pixels = bitmapOfByteString sizeX sizeY (BitmapFormat (TopToBottom
                 f a True  = a ++ onCollor
                 f a _     = a ++ offCollor
 
+{-  window settings
+    Creates a display from settings
+-}
 window :: DisplaySettings -> Display
 window s = InWindow (name s) (size s) (0,0)
 
+{-  renderer settings func cpu
+    Applies createFrame to the pixels created from applying func to cpu
+    PRE: cpu is in a functional state
+-}
 renderer :: DisplaySettings -> RendererFunc -> CPU -> Picture
 renderer s f state = createFrame s $ f state
 
+{-  handleKeys func event cpu
+    Applies func to cpu whenever a key pressed event is called
+    PRE: cpu is in a functional state
+-}
 handleKeys :: HandleKeyFunc -> Event -> CPU -> CPU
 handleKeys f (EventKey (Char key) Down _ _) game = f key game
 handleKeys _ _ game = game
 
+{-  startRenderer settings cpu rFunc hFunc uFunc
+    Starts a game
+    PRE: cpu is in a functional state,
+         The number of pixels is equal to the number of pixels required for the given screen size from settings
+    SIDE EFFECTS: Creates a window where the screen is drawn
+                  Updates the screen every frame
+                  Calls rFunc every frame
+                  Calls uFunc every frame
+                  Calls hFunc everytime a key is pressed
+-}
 startRenderer :: DisplaySettings -> CPU -> RendererFunc -> HandleKeyFunc -> UpdateFunc -> IO()
 startRenderer s gS rF hF uF = play (window s) (background s) (fps s) gS (renderer s rF) (handleKeys hF) uF 
-
-{-
-    Examples: for 0 (<= 4) (+1) $ do (putStrLn "1") == "1 \n 1 \n 1 \n 1" 
--}
-for :: (Eq a) => a -> (a -> Bool) -> (a -> a) -> IO () -> IO ()
-for a eqs eqa io = do
-    if (eqs a)
-    then do 
-        io
-        for (eqa a) eqs eqa io
-    else do (return ())

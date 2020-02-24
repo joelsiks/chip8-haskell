6d812f25bd4622bb601cc538bf7427748d510aba
JoXW100 <joxw100@gmail.com>
2020-02-24 15:03:24 +0100

    Started implementing renderer in main, removed unnecessary types in renderer and other fixes

--- src/Render/Renderer.hs
+++ src/Render/Renderer.hs
@@ -1,4 +1,4 @@
-module Render.Renderer (DisplaySettings,HandleKeyFunc,UpdateFunc,RendererFunc,startRenderer) where
+module Render.Renderer (DisplaySettings(..), startRenderer) where
 
 import CPU.CPU
 import Graphics.Gloss
@@ -7,23 +7,6 @@ import Graphics.Gloss.Interface.IO.Game
 import Data.Word
 import Data.ByteString (ByteString, pack) -- Same as a bitmap
 
-{-  Represents the type of a function that handles user inputs for a game.
-    When a game is started it requires some function that handles inputs, that function has this type
-    INVARIANT: used for functions asociated with startRenderer
--}
-type HandleKeyFunc = Char -> CPU -> CPU
-
-{-  Represents the type of function that handles update calls from a game
-    When a game is started it requires some function that handles what to do every frame, that function has this type
-    INVARIANT: used for functions asociated with startRenderer
--}
-type UpdateFunc    = Float -> CPU -> CPU
-
-{-  Represents the type of function that handles conversion from a CPU state to an image
-    When a game is started it requires some function that handles how to convert a CPU state into an image, that function has this type
-    INVARIANT: used for functions asociated with startRenderer
--}
-type RendererFunc  = CPU -> [[Bool]]
 
 {-  Represents a holder for all display variables
     Stores variables used for the creaton of a display
@@ -32,7 +15,6 @@ type RendererFunc  = CPU -> [[Bool]]
 data DisplaySettings = Settings
     {
         name       :: String,
-        size       :: (Int, Int),
         background :: Color,
         fps        :: Int
     } deriving (Show)
@@ -43,9 +25,8 @@ data DisplaySettings = Settings
     RETURNS: A renderer readable picture created from the given pixels
 -}
 createFrame :: DisplaySettings -> [[Bool]] -> Picture
-createFrame s pixels = bitmapOfByteString sizeX sizeY (BitmapFormat (TopToBottom) (PxRGBA)) bitmapData False
+createFrame s pixels = bitmapOfByteString 64 32 (BitmapFormat (TopToBottom) (PxRGBA)) bitmapData False
     where
-        (sizeX, sizeY) = (size s)
         bitmapData = createBitmapData $ concat pixels
 
         createBitmapData :: [Bool] -> ByteString
@@ -62,20 +43,20 @@ createFrame s pixels = bitmapOfByteString sizeX sizeY (BitmapFormat (TopToBottom
     Creates a display from settings
 -}
 window :: DisplaySettings -> Display
-window s = InWindow (name s) (size s) (0,0)
+window s = InWindow (name s) (64,32) (0,0)
 
 {-  renderer settings func cpu
     Applies createFrame to the pixels created from applying func to cpu
     PRE: cpu is in a functional state
 -}
-renderer :: DisplaySettings -> RendererFunc -> CPU -> Picture
+renderer :: DisplaySettings -> (CPU -> [[Bool]]) -> CPU -> Picture
 renderer s f state = createFrame s $ f state
 
 {-  handleKeys func event cpu
     Applies func to cpu whenever a key pressed event is called
     PRE: cpu is in a functional state
 -}
-handleKeys :: HandleKeyFunc -> Event -> CPU -> CPU
+handleKeys :: (Char -> CPU -> CPU) -> Event -> CPU -> CPU
 handleKeys f (EventKey (Char key) Down _ _) game = f key game
 handleKeys _ _ game = game
 
@@ -89,5 +70,5 @@ handleKeys _ _ game = game
                   Calls uFunc every frame
                   Calls hFunc everytime a key is pressed
 -}
-startRenderer :: DisplaySettings -> CPU -> RendererFunc -> HandleKeyFunc -> UpdateFunc -> IO()
+startRenderer :: DisplaySettings -> CPU -> (CPU -> [[Bool]]) -> (Char -> CPU -> CPU) -> (Float -> CPU -> CPU) -> IO()
 startRenderer s gS rF hF uF = play (window s) (background s) (fps s) gS (renderer s rF) (handleKeys hF) uF 
--- src/main.hs
+++ src/main.hs
@@ -2,6 +2,8 @@ module Main where
 
 import CPU.CPU
 import CPU.LoadRom
+import Render.Renderer
+import Graphics.Gloss(blue)
 
 import System.Random
 
@@ -11,4 +13,20 @@ main = do
     rom <- CPU.LoadRom.readRom testPath
     let cpu = CPU.CPU.initCPU rom (mkStdGen 0)
     -- skicka vidare CPU här
-    putStrLn "Hello, Haskell!"
+
+    let displaySettings = Settings "Test" blue 60
+    startRenderer displaySettings cpu onRender onInput onUpdate
+
+-- Called last every frame
+onRender :: CPU -> [[Bool]]
+onRender cpu = undefined
+
+-- Celled on input
+onInput :: Char -> CPU -> CPU
+onInput key cpu = undefined
+
+-- Called every frame before onRenderer
+onUpdate :: Float -> CPU -> CPU
+onUpdate time cpu = undefined
+
+

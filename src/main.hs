a02fb5d7dc3b29582ba4a1b3328ad2255ebc60e6
Casper <caspernorrbin1@gmail.com>
2020-02-24 14:48:52 +0100

    added testPaths for reading roms

--- src/main.hs
+++ src/main.hs
@@ -7,8 +7,10 @@ import System.Random
 
 main :: IO ()
 main = do
-    let testPath = "../roms/PONG"
-    rom <- CPU.LoadRom.readRom testPath
+    let ghciTestPath = "../roms/PONG"
+    let cabalRunTestPath = "roms/PONG"
+    rom <- CPU.LoadRom.readRom cabalRunTestPath
     let cpu = CPU.CPU.initCPU rom (mkStdGen 0)
+    -- bätre slumpgenerering
     -- skicka vidare CPU här
     putStrLn "Hello, Haskell!"

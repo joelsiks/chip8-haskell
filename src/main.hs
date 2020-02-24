cab2aa63a0296f9658e28ccd827b132ef9ec0936
Casper <caspernorrbin1@gmail.com>
2020-02-24 14:30:29 +0100

    primitive CPU initialization

--- src/main.hs
+++ src/main.hs
@@ -1,6 +1,14 @@
 module Main where
 
 import CPU.CPU
+import CPU.LoadRom
+
+import System.Random
 
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+    let testPath = "../roms/PONG"
+    rom <- CPU.LoadRom.readRom testPath
+    let cpu = CPU.CPU.initCPU rom (mkStdGen 0)
+    -- skicka vidare CPU här
+    putStrLn "Hello, Haskell!"

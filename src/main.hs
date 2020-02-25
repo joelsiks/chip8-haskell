216f78752c5a74f42b1c484fcb1bbffa5d417ae3
Joel Sikström <joel.attano@gmail.com>
2020-02-25 10:53:19 +0100

    Indentation fix.

--- src/main.hs
+++ src/main.hs
@@ -2,22 +2,23 @@ module Main where
 
 import CPU.CPU
 import CPU.LoadRom
+import qualified CPU.Utility as Util
 import Render.Renderer
 import Graphics.Gloss(blue)
 import System.Random
 
 main :: IO ()
 main = do
-    let ghciTestPath = "../roms/PONG"
-    let cabalRunTestPath = "roms/PONG"
-    rom <- CPU.LoadRom.readRom cabalRunTestPath
-    let cpu = CPU.CPU.initCPU rom (mkStdGen 0)
-    -- bätre slumpgenerering
-    -- skicka vidare CPU här
-    putStrLn "Hello, World!"
-
-    --let displaySettings = Settings "Test" blue 60
-    --startRenderer displaySettings cpu onRender onInput onUpdate
+  let ghciTestPath = "../roms/PONG"
+  let cabalRunTestPath = "roms/PONG"
+  rom <- CPU.LoadRom.readRom cabalRunTestPath
+  let cpu = CPU.CPU.initCPU rom (mkStdGen 0)
+  -- bätre slumpgenerering
+  -- skicka vidare CPU här
+  putStrLn "Hello, World!"
+  
+  --let displaySettings = Settings "Test" blue 60
+  --startRenderer displaySettings cpu onRender onInput onUpdate
 
 -- Called last every frame
 onRender :: CPU -> [Int]
@@ -26,33 +27,26 @@ onRender cpu = concat (vram cpu)
 -- Celled on input
 onInput :: Char -> CPU -> CPU
 onInput key cpu = cpu {keyboard = setKeyTrue key (keyboard cpu)}
-    where
-        setKeyTrue :: Char -> [Bool] -> [Bool]
-        setKeyTrue '1' keys = setAt 0 keys True
-        setKeyTrue '2' keys = setAt 1 keys True
-        setKeyTrue '3' keys = setAt 2 keys True
-        setKeyTrue '4' keys = setAt 3 keys True
-        setKeyTrue 'q' keys = setAt 4 keys True
-        setKeyTrue 'w' keys = setAt 5 keys True
-        setKeyTrue 'e' keys = setAt 6 keys True
-        setKeyTrue 'r' keys = setAt 7 keys True
-        setKeyTrue 'a' keys = setAt 8 keys True
-        setKeyTrue 's' keys = setAt 9 keys True
-        setKeyTrue 'd' keys = setAt 10 keys True
-        setKeyTrue 'f' keys = setAt 11 keys True
-        setKeyTrue 'z' keys = setAt 12 keys True
-        setKeyTrue 'x' keys = setAt 13 keys True
-        setKeyTrue 'c' keys = setAt 14 keys True
-        setKeyTrue 'v' keys = setAt 15 keys True
-        setKeyTrue _ keys = keys
+  where
+    setKeyTrue :: Char -> [Bool] -> [Bool]
+    setKeyTrue '1' keys = Util.replace 0x0 True keys
+    setKeyTrue '2' keys = Util.replace 0x1 True keys
+    setKeyTrue '3' keys = Util.replace 0x2 True keys
+    setKeyTrue '4' keys = Util.replace 0x3 True keys
+    setKeyTrue 'q' keys = Util.replace 0x4 True keys
+    setKeyTrue 'w' keys = Util.replace 0x5 True keys
+    setKeyTrue 'e' keys = Util.replace 0x6 True keys
+    setKeyTrue 'r' keys = Util.replace 0x7 True keys
+    setKeyTrue 'a' keys = Util.replace 0x8 True keys
+    setKeyTrue 's' keys = Util.replace 0x9 True keys
+    setKeyTrue 'd' keys = Util.replace 0xA True keys
+    setKeyTrue 'f' keys = Util.replace 0xB True keys
+    setKeyTrue 'z' keys = Util.replace 0xC True keys
+    setKeyTrue 'x' keys = Util.replace 0xD True keys
+    setKeyTrue 'c' keys = Util.replace 0xE True keys
+    setKeyTrue 'v' keys = Util.replace 0xF True keys
+    setKeyTrue _ keys = keys
 
 -- Called every frame before onRenderer
 onUpdate :: Float -> CPU -> CPU
 onUpdate time cpu = cpu
-
-{-  setAt index list elem
-    PRE: index >= 0 && length list > index
--}
-setAt :: Int -> [a] -> a -> [a]
-setAt 0 (_:xs) z = z : xs
-setAt v (x:xs) z = x : (setAt (v-1) xs z)
